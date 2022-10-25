422bb392220ac79ff92e432a50bc1f96e521d317
Duane David <duanedave17@gmail.com>
2022-10-25 07:51:18 -0700

    Fix bugs on med DB

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -13,21 +13,23 @@ CREATE TABLE medical_histories (
 
 CREATE TABLE invoices (
   id BIGSERIAL PRIMARY KEY,
-  total_amount DECIMAL() NOT NULL,
+  total_amount DECIMAL NOT NULL,
   generated_at TIMESTAMP NOT NULL,
-  payed_at TIMESTAMP
-  medical_history_id INT REFERENCES medical_histories(id),
+  payed_at TIMESTAMP,
+  medical_history_id INT REFERENCES medical_histories(id)
 );
 
 CREATE TABLE treatments (
-  id INT REFERENCES medical_histories(id),
+  id BIGSERIAL REFERENCES medical_histories(id) PRIMARY KEY,
   type VARCHAR(200) NOT NULL,
   name VARCHAR(200) NOT NULL
 );
 
 CREATE TABLE invoice_items (
-  id INT REFERENCES invoices(id) PRIMARY KEY,
-  treatement_id INT REFERENCES treatments,
-  type VARCHAR(200) NOT NULL,
-  name VARCHAR(200) NOT NULL
+  id BIGSERIAL PRIMARY KEY,
+  unit_price DECIMAL NOT NULL,
+  quantity INT NOT NULL,
+  total_price DECIMAL NOT NULL,
+  invoice_id INT REFERENCES invoices(id) NOT NULL,
+  treatement_id INT REFERENCES treatments(id)
 );
