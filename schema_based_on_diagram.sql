67919757585009ba97418fa88c9b9a5020a6b4d9
aeh1707d <abdelhadi.hireche@outlook.com>
2022-10-25 15:33:34 +0100

    Making connections

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,32 +1,33 @@
 CREATE TABLE patients (
   id BIGSERIAL PRIMARY KEY,
   name VARCHAR(200) NOT NULL,
-  date_of_birth DATE NOT NULL,
+  date_of_birth DATE NOT NULL
 );
 
 CREATE TABLE medical_histories (
   id BIGSERIAL PRIMARY KEY,
   admitted_at TIMESTAMP NOT NULL,
   patient_id INT REFERENCES patients(id) NOT NULL,
-  status VARCHAR(200) NOT NULL,
+  status VARCHAR(200) NOT NULL
 );
 
 CREATE TABLE invoices (
   id BIGSERIAL PRIMARY KEY,
   total_amount DECIMAL() NOT NULL,
   generated_at TIMESTAMP NOT NULL,
-  payed_at TIMESTAMP,
-  medical_history_id INT NOT NULL,
-);
-
-CREATE TABLE invoice_items (
-  id BIGSERIAL REFERENCES invoices(id) PRIMARY KEY,
-  type VARCHAR(200) NOT NULL,
-  name VARCHAR(200) NOT NULL,
+  payed_at TIMESTAMP
+  medical_history_id INT REFERENCES medical_histories(id),
 );
 
 CREATE TABLE treatments (
-  id BIGSERIAL PRIMARY KEY,
+  id INT REFERENCES medical_histories(id),
   type VARCHAR(200) NOT NULL,
-  name VARCHAR(200) NOT NULL,
-)
+  name VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE invoice_items (
+  id INT REFERENCES invoices(id) PRIMARY KEY,
+  treatement_id INT REFERENCES treatments,
+  type VARCHAR(200) NOT NULL,
+  name VARCHAR(200) NOT NULL
+);
